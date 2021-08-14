948316c3dd8c70d84ef9999e6c49da43710dfe1a
dayojohn19 <dayo_john16@yahoo.com>
2021-08-14 22:08:04 +0800

    added tutorials

---
+++ index.js
@@ -0,0 +1,6 @@
+
+
+
+
+function git_repositories(){var gr = document.querySelector('.git_repositories');var gf = document.querySelector('.git_fork');gr.style.display = 'block';gf.style.display = 'none';}
+function git_fork (){var gr = document.querySelector('.git_repositories');var gf = document.querySelector('.git_fork');gf.style.display = 'block';gr.style.display = 'none';}
