dbcb69b57e15a0d7229e7b25910dfbf156e54614
dayojohn19 <dayo_john16@yahoo.com>
2021-08-16 20:42:55 +0800

    added js

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
 
 
 var wel = document.querySelector('.welcome');
-document.addEventListener('DOMContentLoaded', ()=>{
-    wel.style.display = 'block';
-});
+// document.addEventListener('DOMContentLoaded', ()=>{
+//     wel.style.display = 'block';
+// });
 
 window.onclick = function(event) {
     if (event.target == wel) {
@@ -11,7 +11,28 @@ window.onclick = function(event) {
     }
   }
 
-function git_repositories(){var gr = document.querySelector('.git_repositories');var gf = document.querySelector('.git_fork');gr.style.display = 'block';gf.style.display = 'none';}
-function git_fork (){var gr = document.querySelector('.git_repositories');var gf = document.querySelector('.git_fork');gf.style.display = 'block';gr.style.display = 'none';}
+function git_repositories(){
+var gr = document.querySelector('.git_repositories');
+var gf = document.querySelector('.git_fork');
+var js = document.querySelector('.javascript');
+js.style.display = 'none';
+gr.style.display = 'block';
+gf.style.display = 'none';}
+
+function git_fork (){
+var gr = document.querySelector('.git_repositories');
+var gf = document.querySelector('.git_fork');
+var js = document.querySelector('.javascript');
+js.style.display = 'none';
+gf.style.display = 'block';
+gr.style.display = 'none';}
+
+function javascript (){var gr = document.querySelector('.javascript');
+var gr = document.querySelector('.git_repositories');
+var gf = document.querySelector('.git_fork');
+var js = document.querySelector('.javascript');
+gf.style.display = 'none';
+gr.style.display = 'none';
+js.style.display = 'block';}
 
 var mybutton = document.getElementById("myBtn");window.onscroll = function() {scrollFunction()};function scrollFunction() {if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {mybutton.style.display = "block";} else {mybutton.style.display = "none";}}function topFunction() {document.body.scrollTop = 0;document.documentElement.scrollTop = 0;}
