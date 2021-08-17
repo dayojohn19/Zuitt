9ca4d25dfa57cde2c2ffa9bfde3c0c4b9e41baf3
dayojohn19 <dayo_john16@yahoo.com>
2021-08-17 14:00:01 +0800

    Include JS Selection COntrol activity

---
+++ sessions/s16/a1/index.js
@@ -0,0 +1,18 @@
+function submit() {
+    f = document.querySelector("#first").value;
+    l = document.querySelector("#last").value;
+    e = document.querySelector("#email").value;
+    p1 = document.querySelector("#password").value;
+    p2 = document.querySelector("#password2").value;
+
+    if (p1 != p2){
+        alert("Password Must Match");
+    }else {
+        console.log(f);
+        console.log(l);
+        console.log(e);
+        console.log(p1);
+        console.log(p2);
+        alert("thanks for Signing up");
+    }
+}
