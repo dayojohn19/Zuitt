d445110d3d0e16b2e83071010c8f253ba1215069
dayojohn19 <dayo_john16@yahoo.com>
2021-09-23 11:35:59 +0800

    d

--- sessions/s54/ss54/queue.js
+++ sessions/s54/ss54/queue.js
@@ -2,35 +2,13 @@ let collection = [];
 
 // Write the queue functions below.
 function print () {
-    // x = print(c);
     return collection
 }
 
 function enqueue (x) {
     c = collection.push(x);
-    // collection = Array.from(c);
     return collection
 }
-
-// function enqueue (x) {
-//     c = {0:'Jane', 1:'Bob',3:'Cherry'};
-//     xx = Object.values(c);
-//     // z = xx.length;
-//     e = (xx.indexOf(x));
-//     z = xx.filter(xx => xx.length == e);
-//     // d = c[e];
-//     return z
-// }
-
-// function enqueue (x) {
-//     // c = {0:'Jane', 1:'Bob',3:'Cherry'}`;
-//     cc = ['jane','Bob','Cherry'];
-//     z = cc.splice(cc => cc.length == x);
-//     zz = cc.splice(x)
-//     return zz
-// }
-// // 
-
 
 
 function dequeue (x) {
